ad8d2cd9d88fc7df0bae1a49a446e41aaa58c835
cudillal <leopold.cudilla@ecrin.org>
2024-04-18 10:53:16 +0200

    Move clientId to environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,10 @@ import { appVersion } from './version';
 
 export const environment = {
   production: false,
+  clientId: 'APP-45A6EC5D-8206-4356-A105-2AFE5FA7A831',
+  // clientId: 'APP-FDD7EABC-450A-426B-BDFA-3CBD87B7C56A',  // CRR one that works with localhost incase the other doesn't let you log in
+  // clientId: 'APP-B43C4910-51CB-4A97-A16A-D611E48EF418',  // Note: unknown client ID Error
+  // client id seen in https://services.aai.lifescience-ri.eu/spreg/auth/facilities/detail/3955 : 7cf4d894-7b95-4daf-b80f-96a350b2980d
   appVersion: appVersion,
   appTitle: 'crDSR: Data Sharing Repository',
   baseUrl: 'https://api-dsrdev.ecrin.org/api',
