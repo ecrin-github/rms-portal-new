255b2b44fdc6f23cfbe9ea0e4340654293b4d5e9
cudillal <leopold.cudilla@ecrin.org>
2024-06-17 16:33:50 +0200

    Fix MDR URL

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -37,7 +37,7 @@ export class AuthComponent implements OnInit {
   }
   goToMdr() {
       this.router.navigate([])
-      .then(result => { window.open('https://crmdr.org/', '_blank'); });
+      .then(result => { window.open('https://crmdr.ecrin.org/', '_blank'); });
   }
   goToLegalNotice() {
     this.router.navigate([])
